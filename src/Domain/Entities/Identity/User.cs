66b159f5ef8ab544d093e5a2e6ee4ae50be7e559
Taufan Augusta <taufan.augusta@protonmail.com>
2022-08-02 22:29:18 +0700

    refactor: add ctor

--- src/Domain/Entities/Identity/User.cs
+++ src/Domain/Entities/Identity/User.cs
@@ -2,11 +2,22 @@ namespace Onion.Domain.Entities.Identity;
 
 public class User
 {
+    public User(string username, string email, string role, string password, string salt)
+    {
+        Id = Guid.NewGuid();
+        Username = username;
+        Email = email;
+        Role = role;
+        IsVerified = false;
+        Password = password;
+        Salt = salt;
+    }
+
     public Guid Id { get; set; }
-    public string Username { get; set; } = null!;
-    public string Email { get; set; } = null!;
-    public string Role { get; set; } = null!;
+    public string Username { get; set; }
+    public string Email { get; set; }
+    public string Role { get; set; }
     public bool IsVerified { get; set; }
-    public string Password { get; set; } = null!;
-    public string Salt { get; set; } = null!;
+    public string Password { get; set; }
+    public string Salt { get; set; }
 }

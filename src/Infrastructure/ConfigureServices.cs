d8bd010e3fc2cd61fdee3f532e7d493e03c7b7d6
Taufan Augusta <taufan.augusta@protonmail.com>
2022-08-02 23:03:30 +0700

    inject secure hash

--- src/Infrastructure/ConfigureServices.cs
+++ src/Infrastructure/ConfigureServices.cs
@@ -2,7 +2,9 @@ using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Onion.Application.Common.Interfaces;
+using Onion.Application.Common.Services;
 using Onion.Infrastructure.Persistence;
+using Onion.Infrastructure.Services;
 
 namespace Onion.Infrastructure;
 
@@ -10,6 +12,7 @@ public static class ConfigureServices
 {
     public static IServiceCollection AddInfrastructureServices(this IServiceCollection services, IConfiguration configuration)
     {
+        services.AddSingleton<ISecureHashProvider, SecureHashProvider>();
         services.AddDbContext<IDbContext, ApplicationDbContext>(options =>
         {
             options.UseInMemoryDatabase(nameof(ApplicationDbContext));

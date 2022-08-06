40a7160291ad92705eff93c43fe71e12bcefc982
Taufan Augusta <taufan.augusta@protonmail.com>
2022-08-06 21:22:45 +0700

    feat(api): get all users

---
+++ src/Api/Endpoints/Identity.Management/UserManagementEndpoint.cs
@@ -0,0 +1,32 @@
+using Onion.Application.Identity.Management.Queries.GetAllUsers;
+
+namespace Onion.Api.Endpoints.Identity.Management;
+
+public class UserManagementEndpoint : IEndpoint
+{
+    private const string GetAllUserUri = "api/user/get-all";
+
+    public void DefineEndpoints(WebApplication app)
+    {
+        app.MapGet(GetAllUserUri, GetAllUser);
+    }
+
+    internal async Task<IResult> GetAllUser([FromServices] ISender sender,
+        HttpContext httpContext)
+    {
+        var query = new GetAllUsersQuery();
+        var getAllUser = await sender.Send(query);
+
+        if(getAllUser.Success)
+        {
+            var value = getAllUser.Value;
+            return Results.Ok(value);
+        }
+        else
+        {
+            int code = (int)HttpStatusCode.NotFound;
+            var problemDetails = getAllUser.ToProblemDetails(GetAllUserUri, code, httpContext);
+            return Results.Problem(problemDetails);
+        }
+    }
+}

6960ba7648e271260c6d3506c7711de76062f92c
Taufan Augusta <taufan.augusta@protonmail.com>
2022-08-11 01:38:07 +0700

    add (internal) jwt claim types

---
+++ src/Infrastructure/Authentication/JwtClaimTypes.cs
@@ -0,0 +1,21 @@
+namespace Onion.Infrastructure.Authentication;
+
+public static class JwtClaimTypes
+{
+    public const string Jti = "jti";
+    public const string Sub = "sub";
+    public const string Aud = "aud";
+    public const string Iss = "iss";
+    public const string Exp = "exp";
+    public const string Nbf = "nbf";
+    public const string Iat = "iat";
+    public const string acr = "arc";
+    public const string amr = "amr";
+    public const string azp = "azp";
+    public const string AuthTime = "auth_time";
+    public const string Nonce = "nonce";
+    public const string UserId = "user_id";
+    public const string UniqueName = "unique_name";
+    public const string Email = "email";
+    public const string Role = "role";
+}

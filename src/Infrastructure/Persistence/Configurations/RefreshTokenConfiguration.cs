7316ab76f46892dc8ceec3af53a7a97f007b0808
Taufan Augusta <taufan.augusta@protonmail.com>
2022-08-21 17:43:32 +0700

    chore: wrap and align chain call

--- src/Infrastructure/Persistence/Configurations/RefreshTokenConfiguration.cs
+++ src/Infrastructure/Persistence/Configurations/RefreshTokenConfiguration.cs
@@ -11,42 +11,42 @@ public class RefreshTokenConfiguration : IEntityTypeConfiguration<RefreshToken>
         builder.ToTable("dbo.refresh_token");
 
         builder.Property(x => x.Id)
-            .HasColumnName("token_id")
-            .ValueGeneratedOnAdd()
-            .HasMaxLength(36)
-            .IsRequired();
+               .HasColumnName("token_id")
+               .ValueGeneratedOnAdd()
+               .HasMaxLength(36)
+               .IsRequired();
 
         builder.Property(x => x.Token)
-            .HasColumnName("token")
-            .IsRequired();
+               .HasColumnName("token")
+               .IsRequired();
 
         builder.Property(x => x.JwtId)
-            .HasColumnName("jwt_id")
-            .IsRequired();
+               .HasColumnName("jwt_id")
+               .IsRequired();
 
         builder.Property(x => x.CreationDate)
-            .HasColumnName("creation_date")
-            .IsRequired();
+               .HasColumnName("creation_date")
+               .IsRequired();
 
         builder.Property(x => x.ExpiryDate)
-            .HasColumnName("expiry_date")
-            .IsRequired();
+               .HasColumnName("expiry_date")
+               .IsRequired();
 
         builder.Property(x => x.IsUsed)
-            .HasColumnName("is_used")
-            .IsRequired();
+               .HasColumnName("is_used")
+               .IsRequired();
 
         builder.Property(x => x.IsInvalidated)
-            .HasColumnName("is_invalidated")
-            .IsRequired();
+               .HasColumnName("is_invalidated")
+               .IsRequired();
 
         builder.Property(x => x.UserId)
-            .HasColumnName("user_id")
-            .IsRequired();
+               .HasColumnName("user_id")
+               .IsRequired();
 
         builder.HasOne(x => x.User)
-            .WithMany()
-            .IsRequired()
-            .OnDelete(DeleteBehavior.Cascade);
+               .WithMany()
+               .IsRequired()
+               .OnDelete(DeleteBehavior.Cascade);
     }
 }
--- src/Infrastructure/Persistence/Configurations/UserConfiguration.cs
+++ src/Infrastructure/Persistence/Configurations/UserConfiguration.cs
@@ -12,40 +12,40 @@ internal sealed class UserConfiguration : IEntityTypeConfiguration<User>
         builder.ToTable("dbo.user");
 
         builder.Property(x => x.Id)
-            .HasColumnName("user_id")
-            .ValueGeneratedNever()
-            .HasMaxLength(36)
-            .IsRequired();
+               .HasColumnName("user_id")
+               .ValueGeneratedNever()
+               .HasMaxLength(36)
+               .IsRequired();
 
         builder.Property(x => x.Username)
-            .HasColumnName("username")
-            .HasMaxLength(16)
-            .IsRequired();
+               .HasColumnName("username")
+               .HasMaxLength(16)
+               .IsRequired();
 
         builder.Property(x => x.Email)
-            .HasColumnName("email")
-            .HasMaxLength(40)
-            .IsRequired();
+               .HasColumnName("email")
+               .HasMaxLength(40)
+               .IsRequired();
 
         builder.Property(x => x.Role)
-            .HasColumnName("role")
-            .HasConversion(
-                v => v.Value,
-                v => UserRole.FromValue(v))
-            .IsRequired();
+               .HasColumnName("role")
+               .HasConversion(
+                    v => v.Value,
+                    v => UserRole.FromValue(v))
+               .IsRequired();
 
         builder.Property(x => x.IsVerified)
-            .HasColumnName("is_verified")
-            .IsRequired();
+               .HasColumnName("is_verified")
+               .IsRequired();
 
         builder.Property(x => x.HashedPassword)
-            .HasColumnName("hashed_password")
-            .HasMaxLength(96) // SHA384
-            .IsRequired();
+               .HasColumnName("hashed_password")
+               .HasMaxLength(96) // SHA384
+               .IsRequired();
 
         builder.Property(x => x.Salt)
-            .HasColumnName("salt")
-            .HasMaxLength(8)
-            .IsRequired();
+               .HasColumnName("salt")
+               .HasMaxLength(8)
+               .IsRequired();
     }
 }
